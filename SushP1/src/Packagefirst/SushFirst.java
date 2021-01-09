95e728747ae83f27b53beb7b2868c5ac4a00e9a8
sushantisingh <sushantisingh@sushanti>
2021-01-09 12:40:20 +0530

    this is for testing purpose

---
+++ SushP1/src/Packagefirst/SushFirst.java
@@ -0,0 +1,16 @@
+package Packagefirst;
+
+public class SushFirst 
+{
+	int a;
+	public void method1()
+	{
+		System.out.println("Hey people, I m here");
+	}
+   public static void main(String[] args) {
+	   SushFirst nidhi=new SushFirst();
+	   nidhi.method1();
+	   nidhi.a=123;
+	   System.out.println(nidhi.a);
+}
+}
